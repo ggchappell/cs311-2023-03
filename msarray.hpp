cc7464730b70a5ea554555f87f83d0299a8aa7fb
Glenn G. Chappell <ggchappell@alaska.edu>
2023-10-17 23:07:59 -0800

    Add msarray.hpp/.cpp (UNFINISHED)

---
+++ msarray.hpp
@@ -0,0 +1,50 @@
+// msarray.hpp  UNFINISHED
+// Glenn G. Chappell
+// 2023-10-17
+//
+// For CS 311 Fall 2023
+// Header for class MSArray
+// Marvelously smart array of int
+// Preliminary to Assignment 5
+
+#ifndef FILE_MSARRAY_HPP_INCLUDED
+#define FILE_MSARRAY_HPP_INCLUDED
+
+
+// *********************************************************************
+// class MSArray - Class definition
+// *********************************************************************
+
+
+// class MSArray
+// Marvelously Smart Array of int.
+// Resizable, copyable/movable, exception-safe.
+// Invariants:
+//     ???
+class MSArray {
+
+// ***** MSArray: types *****
+public:
+
+
+// ***** MSArray: ctors, op=, dctor *****
+public:
+
+
+// ***** MSArray: general public operators *****
+public:
+
+
+// ***** MSArray: general public functions *****
+public:
+
+
+// ***** MSArray: data members *****
+private:
+
+
+};  // End class MSArray
+
+
+#endif  //#ifndef FILE_MSARRAY_HPP_INCLUDED
+
